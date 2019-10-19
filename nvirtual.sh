d77428c1c2440f91fccaeeb54b75c77c0fa75b07
Ousseni bara <33071371+iamousseni@users.noreply.github.com>
2019-10-19 18:36:29 +0200

    Update nvirtual.sh

--- nvirtual.sh
+++ nvirtual.sh
@@ -12,13 +12,36 @@ echo -e "\e[32m\e[1mOK!"
 echo -e "\e[94m\e[1mI'm creating a sample page for \e[4m$virtualName\e[0m\e[94m\e[1m"
 touch /var/www/html/$virtualName/index.php
 /bin/cat <<EOM >/var/www/html/$virtualName/index.php
-<html>
- <head>
- <title>$virtualName</title>
- </head>
- <body>
- <p>Hello, This is a test page for <strong>$virtualName</strong> website</p>
- </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous">
+
+    <title>$virtualName!</title>
+</head>
+<body>
+<p>This is a sample page of <strong>$virtualName</strong> website </p>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+</body>
 </html>
 EOM
 echo -e "\e[32m\e[1mOK!"
